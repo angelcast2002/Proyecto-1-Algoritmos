7e8bf9232d9009781d7bae2dc3f2d4a6668409ee
Ángel Castellanos <77862762+angelcast2002@users.noreply.github.com>
2022-03-10 10:19:37 -0600

    Implementacion de las expresiones regulares para las aritmeticas

---
+++ .history/Scanner_20220310101730.java
@@ -0,0 +1,35 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Scanner {
+    public static int State(String expresion) {
+
+        // Se crea la expresion para la suma de enteros
+        if (evaluate("^[(][ ]*[+][ ]+([a-z]+|[0-9]+)[ ]+([a-z]+|[0-9]+)[ ]*[)]$",expresion)) { 
+            return 1;
+        }
+        
+        // Se crea la expresion para la resta de enteros
+        else if (evaluate("^[(][ ]*[-][ ]+([a-z]+|[0-9]+)[ ]+([a-z]+|[0-9]+)[ ]*[)]$",expresion)) {
+            return 2;
+        }
+
+        // Se crea la expresion para la multiplicacion de enteros
+        else if (evaluate("^[(][ ]*[*][ ]+([a-z]+|[0-9]+)[ ]+([a-z]+|[0-9]+)[ ]*[)]$",expresion)) {
+            return 3;
+        }
+
+        // Se crea la expresion para la division de enteros
+        else if (evaluate("^[(][ ]*[/][ ]+([a-z]+|[0-9]+)[ ]+([a-z]+|[0-9]+)[ ]*[)]$",expresion)) {
+            return 4;
+        }
+        
+        return 0;
+    }
+
+    private static boolean evaluate(String regex, String expresion){
+        Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(expresion);
+        return matcher.find();
+    }
+}
---
+++ Interprete.java
@@ -0,0 +1,7 @@
+import java.util.HashMap;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class Interprete {
+    
+}
