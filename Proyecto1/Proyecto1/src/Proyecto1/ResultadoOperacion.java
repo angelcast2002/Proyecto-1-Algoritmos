2065d545ebb663530fec0f5895cc880ec63cb8ec
Ángel Castellanos <77862762+angelcast2002@users.noreply.github.com>
2022-03-10 12:07:55 -0600

    Matcher y suma
    Se crea las expresiones para las expresiones aritmeticas y se empieza a trabajar en la suma

---
+++ Proyecto1/Proyecto1/src/Proyecto1/ResultadoOperacion.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Proyecto1;
+
+/**
+ *
+ * @author Angel
+ */
+public interface ResultadoOperacion {
+
+    /**
+     *
+     */
+    public void performOperation();
+
+    /**
+     *
+     * @param key
+     * @param result
+     */
+    public void addResults(String key, String result);
+}
