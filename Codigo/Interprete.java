138c194aa2a23b77ca8ed756d60a95290f599a74
Sly <cri21631@uvg.edu.gt>
2022-03-23 21:32:51 -0600

    Update Interprete.java

--- Codigo/Interprete.java
+++ Codigo/Interprete.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Interprete {
 
@@ -10,6 +11,7 @@ public class Interprete {
     String instruccion = "";
     
     List<Variable> variables = new ArrayList<>();
+    List<Funciones> funciones = new ArrayList<>();
 
     public void ejecutar() {
 
